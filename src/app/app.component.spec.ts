5f125e3aa636a2e39fefb871c7039368fd2d91dc
Patrick Ineichen <github@frokonet.ch>
2017-05-05 20:35:38 +0200

    Updated npm package dependencies and fixed tslint issues

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ import { By } from '@angular/platform-browser';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
-let TEST_PROVIDERS = [
+const TEST_PROVIDERS = [
   { provide: 'environment', useValue: 'Test' }
 ];
 
@@ -21,9 +21,10 @@ describe('AppComponent', () => {
   it('should display friendly greeter with component name', () => {
     this.fixture.detectChanges();
 
-    let debugElement = this.fixture.debugElement.query(By.css('h2'));
-    let nativeElement = debugElement.nativeElement;
+    const debugElement = this.fixture.debugElement.query(By.css('h2'));
+    const nativeElement = debugElement.nativeElement;
 
-    expect(nativeElement.textContent).toContain('Hi, I\'m AppComponent and I\'m running in Test mode!');
+    expect(nativeElement.textContent)
+      .toContain('Hi, I\'m AppComponent and I\'m running in Test mode!');
   });
 });
